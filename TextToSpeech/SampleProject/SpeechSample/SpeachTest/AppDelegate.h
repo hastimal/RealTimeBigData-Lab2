4820bf5b0e0f53352694ab9f05d8a807ef7bdc76
hastimal <hjdk4@mail.umkc.edu>
2015-09-08 08:25:58 -0500

    Added code for iOS
    Complete code added

---
+++ TextToSpeech/SampleProject/SpeechSample/SpeachTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  
+//  TextToSpeech
+//  Created by Jangid, Hastimal (UMKC-Student) on 9/5/15.
+//  Copyright (c) 2015 UMKC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
